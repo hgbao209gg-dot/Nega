d3da428078cba7c09aef5d5ecca49aced33fff42
hgbao209gg-dot <hgbao209gg@gmail.com>
2025-08-11 10:35:52 +0700

    Create index.ts

---
+++ plugin/index.ts
@@ -0,0 +1,22 @@
+import { storage } from "@vendetta/plugin"
+import patchActionSheet from "./patches/ActionSheet"
+import patchCommands from "./patches/Commands"
+import Settings from "./settings"
+
+export const settings: {
+    gemini_api_key?: string
+    target_lang?: string
+} = storage
+
+settings.target_lang ??= "en"
+
+let patches = []
+
+export default {
+    onLoad: () => patches = [
+        patchActionSheet(),
+        patchCommands()
+    ],
+    onUnload: () => { for (const unpatch of patches) unpatch() },
+    settings: Settings
+}
